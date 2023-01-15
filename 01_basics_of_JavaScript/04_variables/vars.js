69beaf29744a375a9fd2b20e4ecfe5de92858f7f
Kollaider <kolja87@tut.by>
2023-01-15 23:38:49 +0300

    1_4_variables

---
+++ 01_basics_of_JavaScript/04_variables/vars.js
@@ -0,0 +1,15 @@
+let message;
+message = 'hello';
+alert(message);
+
+
+let message2 = 'Hello'
+
+let user = 'John', age = 25, message3 = 'Hello';
+
+let user1 = 'John';
+let age1 = 25;
+let message4 = 'Hello';
+
+
+const myBirthday = '18.04.1982';
