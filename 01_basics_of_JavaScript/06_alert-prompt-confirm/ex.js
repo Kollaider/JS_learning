374c093f88cbf1309e8772259ddd6bc4ad6f9aab
Kollaider <kolja87@tut.by>
2023-01-18 14:47:38 +0300

    1_6_alert-promt-confirm

---
+++ 01_basics_of_JavaScript/06_alert-prompt-confirm/ex.js
@@ -0,0 +1,8 @@
+alert("Hello");
+
+let age = prompt('Сколько тебе лет?', 100);
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+
+let isBoss = confirm("Ты здесь главный?");
+alert( isBoss ); // true, если нажата OK
+
