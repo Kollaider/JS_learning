25625a7fc33079ced1f268ea7726ec5aec8860f5
Kollaider <kolja87@tut.by>
2023-01-18 15:58:04 +0300

    1_8_operators

---
+++ 01_basics_of_JavaScript/08_operators/task_1.js
@@ -0,0 +1,14 @@
+let a1 = "" + 1 + 0             // '10'
+let a2 = "" - 1 + 0             // -1
+let a3 = true + false           // 1
+let a4 = 6 / "3"                // 2
+let a5 = "2" * "3"              // 6
+let a6 = 4 + 5 + "px"           // '9px'
+let a7 = "$" + 4 + 5            // '$45'
+let a8 = "4" - 2                // 2
+let a9 = "4px" - 2              // NaN
+let a10 = "  -9  " + 5          // '  -95
+let a11 = "  -9  " - 5          // -4
+let a12 = null + 1              // 1
+let a13 = undefined + 1         // Nan
+let a14 = " \t \n" - 2          // Nan
